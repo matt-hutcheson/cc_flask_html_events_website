9204fe40073492fbb21d15768ce4d8e0767a76bc
mjmccoach <mmccoach88@gmail.com>
2020-09-24 16:01:03 +0100

    MVP completed and started extensions

--- app/controller.py
+++ app/controller.py
@@ -1,9 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 from app.models.event import *
-from app.models.event_list import event_list
+from app.models.event_list import *
 
 
 @app.route('/')
 def index():
     return render_template("index.html", title="Home", event_list=event_list)
+
+@app.route('/add-event', methods= ["POST"])
+def add_event():
+    name = request.form["name_of_event"]
+    date = request.form["date"]
+    number_of_guests = request.form["number_of_guests"]
+    room_location = request.form["room_location"]
+    description = request.form ["description"]
+    
+    new_event = Event(date, name, number_of_guests, room_location, description)
+    
+    add_new_event(new_event)
+    return redirect('/')
--- app/models/event_list.py
+++ app/models/event_list.py
@@ -4,3 +4,6 @@ event1 = Event("01/01/2020", "Python Intro", 16, "Fishbowl", "Try your hand at p
 event2 = Event("25/12/2020", "Christmas Programming", 10, "Tundra", "Stop coding it's Christmas!")
 
 event_list = [event1, event2]
+
+def add_new_event(event):
+    event_list.append(event)
