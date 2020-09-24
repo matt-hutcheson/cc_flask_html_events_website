266b1b73ba63fd57803318152ca95c2690a15e04
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-24 15:13:13 +0100

    added html and python generator for event list details

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template
+from app.models.event import *
+from app.models.event_list import event_list
+
 
 @app.route('/')
 def index():
-    return "Ok"
+    return render_template("index.html", title="Home", event_list=event_list)
---
+++ app/models/event_list.py
@@ -0,0 +1,6 @@
+from app.models.event import *
+
+event1 = Event("01/01/2020", "Python Intro", 16, "Fishbowl", "Try your hand at python programming, fools!")
+event2 = Event("25/12/2020", "Christmas Programming", 10, "Tundra", "Stop coding it's Christmas!")
+
+event_list = [event1, event2]
